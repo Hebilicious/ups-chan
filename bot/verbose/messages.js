6207478467c81e5c3211cf15cfad38ee01e40a2c
justice <xsh4k3@gmail.com>
2018-01-20 07:11:52 +0100

    Refactoring and adding messages.

--- bot/verbose/messages.js
+++ bot/verbose/messages.js
@@ -1,3 +1,50 @@
+function randomMessage(messages) {
+  return messages[Math.floor(Math.random() * messages.length)]
+}
+
+export function getRandomDontSpoilMessage() {
+  const messages = [
+    "Spoiler are the path to the dark side.",
+    "For the night is long, and full of spoilers.",
+    "This is 10% luck, 20 % skill, 5% pleasure, 50% pain And 100% spoiler. Or not.",
+    "Dumbledore is dead.",
+    "I AM your father!",
+    "No spoiler here.",
+    "Another day at the job son. No spoiler.",
+    "You shall not spoil !",
+    "If I live, spoiler shall die!",
+    "Get down !!!",
+    "I'll protect you from the spoils <3"
+  ]
+  return randomMessage(messages)
+}
+
+export function getRandomLossMessage() {
+  const messages = [
+    "GG, we got zerged.",
+    "NextTime 4sure.",
+    "Bad round.",
+    "Can we redo, I had to scratch my nose?",
+    "Unlucky I guess.",
+    "GG boys... See you next week..."
+  ]
+  return randomMessage(messages)
+}
+
+export function getRandomWinMessage() {
+  const messages = [
+    "Congratulations to @everyone for this insane win !!!",
+    "GG. I hope I was more funny and clever.",
+    "GG Germany won, once again ...",
+    "GG, Welcome to Alustin.",
+    "Croxus got defeated once again! GG.",
+    "GG, big thanks to JLN, once again.",
+    "GG EZ sniped node EZ",
+    "GG boys! See you next week."
+  ]
+  return randomMessage(messages)
+}
+
 export function getRandomOkMessage() {
   const messages = [
     "As you wish.",
@@ -16,10 +63,10 @@ export function getRandomOkMessage() {
     "Right away sir.",
     "Sir, yes sir.",
     "Rush b cyka blyat.. *enables english* Yes mylord.",
-    'Gladly',
-    'Whatever you say.',
+    "Gladly",
+    "Whatever you say.",
     "It's not like i wanted to do that >_<",
     "I would have you right now on this table until you beg for mercy twice."
   ]
-  return messages[Math.floor(Math.random() * messages.length)]
+  return randomMessage(messages)
 }
