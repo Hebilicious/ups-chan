5fac923830d50402b76ef39aeb9ad2e5316f8ec5
Hebilicious <xsh4k3@gmail.com>
2018-01-20 10:56:17 +0100

    EmojiList zzz

--- bot/commands/admin-commands.js
+++ bot/commands/admin-commands.js
@@ -1,4 +1,4 @@
-import * as Meeseeks from "../helpers.js"
+import * as Meeseeks from "../helpers.js";
 
 /**
  * List Emojis the bot has access to.
@@ -8,17 +8,20 @@ import * as Meeseeks from "../helpers.js"
  */
 export function listEmojis(message, client) {
   if (message.content === "$listEmojis") {
-    console.log("listing emojis")
-    const emojiList = client.emojis.map(e => `Name: ${e.name}, ID: ${e.id}`)
-    // console.log(emojiList)
+    console.log("listing emojis");
+    const emojiList = client.emojis.map(
+      e =>
+        `**${client.emojis.get(e.id)} Name**: ${e.name}, **Identifier**: ${
+          e.identifier
+        }, **ID:** ${e.id}`
+    );
 
-    // emojiList.forEach(el => message.channel.send(el.toString()))
-    Meeseeks.superSplit(emojiList.join("\n"), 1500).forEach(el =>
-      message.channel.send(el.toString())
-    )
+    let toSend = Meeseeks.superSplit(emojiList.join("\n"), 2000);
+    console.log(toSend);
+    toSend.forEach(el => message.channel.send(el.toString()));
     // Meeseeks.superArraySplit(Array(50).fill(mock), 500).forEach(el => console.log(el))
   }
 }
 
 let mock =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus sapien augue, eget malesuada elit pretium rutrum. Nunc commodo enim non vestibulum aliquet."
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus sapien augue, eget malesuada elit pretium rutrum. Nunc commodo enim non vestibulum aliquet.";
