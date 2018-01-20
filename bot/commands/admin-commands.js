02dd43906f229e06a0117cba46f4f1d078283feb
justice <xsh4k3@gmail.com>
2018-01-20 10:15:05 +0100

    Fixing length

--- bot/commands/admin-commands.js
+++ bot/commands/admin-commands.js
@@ -12,8 +12,10 @@ export function listEmojis(message, client) {
     const emojiList = client.emojis.map(
       e => `Name: ${e.name}, Identifier: ${e.identifier}, ID: ${e.id}`
     )
-    Meeseeks.superSplit(emojiList, 1000).forEach(el =>
-      message.channel.send(el.toString())
-    )
+    console.log(emojiList)
+    Meeseeks.superSplit(emojiList, 500).forEach(el => message.channel.send(el.toString()))
   }
 }
+
+let mock =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus sapien augue, eget malesuada elit pretium rutrum. Nunc commodo enim non vestibulum aliquet. Maecenas tristique ultrices velit sit amet aliquam. Nam in eros arcu. Donec quis efficitur nulla. Vivamus id bibendum diam. Praesent ut lobortis sem. Nulla non ligula ex. Sed aliquet, neque a tincidunt euismod, tortor augue mollis quam, non sodales erat eros eget odio. Ut eu diam tristique, efficitur neque eu, consequat libero. Nulla tempor orci eu nisi tincidunt, vitae laoreet justo placerat. Nulla sed magna id orci gravida fringilla. Aenean elementum, est nec pharetra euismod, justo lectus consectetur arcu, in cursus risus ex et erat."
