8fda69d1b6c63a410502132e7cf92bdfe54364d1
justice <xsh4k3@gmail.com>
2018-01-20 10:41:37 +0100

    More arrays

--- bot/commands/admin-commands.js
+++ bot/commands/admin-commands.js
@@ -9,13 +9,16 @@ import * as Meeseeks from "../helpers.js"
 export function listEmojis(message, client) {
   if (message.content === "$listEmojis") {
     console.log("listing emojis")
-    const emojiList = client.emojis.map(
-      e => `Name: ${e.name}, Identifier: ${e.identifier}, ID: ${e.id}`
+    const emojiList = client.emojis.map(e => `Name: ${e.name}, ID: ${e.id}`)
+    // console.log(emojiList)
+
+    // emojiList.forEach(el => message.channel.send(el.toString()))
+    Meeseeks.superSplit(emojiList.join("\n"), 1500).forEach(el =>
+      message.channel.send(el.toString())
     )
-    console.log(emojiList)
-    Meeseeks.superSplit(emojiList, 500).forEach(el => message.channel.send(el.toString()))
+    // Meeseeks.superArraySplit(Array(50).fill(mock), 500).forEach(el => console.log(el))
   }
 }
 
 let mock =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus sapien augue, eget malesuada elit pretium rutrum. Nunc commodo enim non vestibulum aliquet. Maecenas tristique ultrices velit sit amet aliquam. Nam in eros arcu. Donec quis efficitur nulla. Vivamus id bibendum diam. Praesent ut lobortis sem. Nulla non ligula ex. Sed aliquet, neque a tincidunt euismod, tortor augue mollis quam, non sodales erat eros eget odio. Ut eu diam tristique, efficitur neque eu, consequat libero. Nulla tempor orci eu nisi tincidunt, vitae laoreet justo placerat. Nulla sed magna id orci gravida fringilla. Aenean elementum, est nec pharetra euismod, justo lectus consectetur arcu, in cursus risus ex et erat."
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus sapien augue, eget malesuada elit pretium rutrum. Nunc commodo enim non vestibulum aliquet."
--- bot/helpers.js
+++ bot/helpers.js
@@ -1,7 +1,16 @@
-export function superSplit(string, n){
- return Array(Math.ceil([...string].length/n)).fill().map((a,i) => [...string].slice(i*n,i*n+n).join(''));
+export function superSplit(string, n) {
+  return Array(Math.ceil(string.length / n))
+    .fill()
+    .map((a, i) => [...string].slice(i * n, i * n + n).join(""))
 }
 
-export function regexSplit(string, n){
-  return string.match(new RegExp(`[\\s\\S]{1,${n}}`, 'g')) || []
+export function superArraySplit(array, n) {
+  let T = array.join("\n")
+  return Array(Math.ceil(T.length / n))
+    .fill()
+    .map((a, i) => array.slice(i * n, i * n + n).join(""))
+}
+
+export function regexSplit(string, n) {
+  return string.match(new RegExp(`[\\s\\S]{1,${n}}`, "g")) || []
 }
