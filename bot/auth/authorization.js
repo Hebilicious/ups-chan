5d0f88223ab3453d6c7a4c03a7d00c533e4dcd4f
Hebilicious <xsh4k3@gmail.com>
2018-02-02 05:26:27 +0100

    Database guild agnostic logic

--- bot/auth/authorization.js
+++ bot/auth/authorization.js
@@ -2,28 +2,24 @@ import * as DB from "../database/database.js"
 
 //Mom&Dad,WeebOverlords,Lider,Test
 
-export const authorizedRolesIds = [
-  "312317944417878016",
-  "341182224139419650",
-  "316283334768459777",
-  "248702697610412032"
-]
+// export const authorizedRolesIds = [
+//   "312317944417878016",
+//   "341182224139419650",
+//   "316283334768459777",
+//   "248702697610412032"
+// ]
 
-export function setAdminRolesIds(guild, roles) {
-  console.log(DB.r.db(guild.id))
-  let conf = DB.Connect(guild).table("configuration")
-  if (conf.adminRoleIds) {
-    console.log("Syncing roles")
-  } else {
-    console.log("Adding roles")
-  }
-  console.log("Updating DB")
-}
-export function getAdminRolesIds(guild) {
-  // console.log(DB.r.db(guild.id))
-  console.log("Getting admin roles")
-  if (conf.adminRoleIds) return conf.adminRolesIds
-}
 export function checkMemberForRolesIds(member, rolesIds) {
   return member.roles.some(r => rolesIds.includes(r.id)) ? true : false
+}
+
+export async function isPrivileged(member) {
+  const conf = await DB.Connect(message.guild)
+    .table("configuration")
+    .get(0)
+    .run()
+  return (
+    member.permissions.has("ADMINISTRATOR") ||
+    checkMemberForRolesIds(member, conf.adminRolesIds)
+  )
 }
--- bot/database/database.js
+++ bot/database/database.js
@@ -1,14 +1,51 @@
-import r from "rethinkdbdash"
-export r from "rethinkdbdash"
+const r = require("rethinkdbdash")()
 
-export async function Connect(guild) {
+/**
+ * Connect to a specified guild database.
+ * @param {Guild} guild
+ */
+export function Connect(guild) {
+  console.log(`Connecting to the DB of ${guild.name}`)
   return r.db(guild.id)
 }
 
-export function syncConnectedServers(client) {
+export function UpdateConfiguration(guild, updates) {
+  console.log("Updating Configuration....")
+  return Connect(guild)
+    .table("configuration")
+    .get(0)
+    .update(updates)
+    .run()
+}
+
+export function UpdateConfigurationArray(guild, key, updates, remove = false) {
+  console.log(key)
+  console.log(updates)
+  console.log(remove)
+  return remove
+    ? Connect(guild)
+        .table("configuration")
+        .get(0)
+        .update({ [key]: r.row(key).difference(updates) })
+        .run()
+    : Connect(guild)
+        .table("configuration")
+        .get(0)
+        .update({ [key]: r.row(key).append(updates) })
+        .run()
+}
+/**
+ * Sync the connected guilds (servers) with the db backend by creating
+ * the tables if they don't exist.
+ * @param {Client} client
+ */
+export async function syncConnectedServers(client) {
+  // console.log(r.dbList())
+  let dbList = await r.dbList().run()
+  console.log(dbList)
   client.guilds.forEach(guild => {
     // console.log(guild.id)
-    if (!r.dbList().includes(guild.id)) {
+    if (!dbList.includes(guild.id)) {
       console.log(`Creating DB ${guild.id} (${guild.name}).`)
       r
         .dbCreate(guild.id)
@@ -29,13 +66,19 @@ export function syncConnectedServers(client) {
                 .insert({
                   id: 0,
                   nodeWarChannel: "",
-                  attendingRole: "",
-                  adminRoleIds: []
+                  attendingRole: "Attending",
+                  adminRolesIds: []
                 })
+                .run()
+                .then(result =>
+                  console.log(
+                    `The fresh DB ${guild.id} (${guild.name}) is ready.`
+                  )
+                )
             })
         })
     } else {
-      console.log(`DB ${guild.id} (${guild.name}) is ready.`)
+      console.log(`Found DB ${guild.id} (${guild.name}), ready to use.`)
     }
   })
 }
