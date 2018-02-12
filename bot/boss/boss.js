4e976e432664388159d1d430704e89cd35f69bc5
psy <psyqo.createch@googlemail.com>
2018-02-12 16:11:59 +0100

    Revert "Universalizing toggleRole function"
    This reverts commit 94f9ac65a656e8aa1efddeb38c158de9767f112c.

--- bot/boss/boss.js
+++ bot/boss/boss.js
@@ -79,12 +79,7 @@ export function handleBoss(client) {
         let channel = guild.channels.find("name", cName)
         if (channel && conf.region == region) {
           console.log(region + " " + boss.alert)
-          if (!role) {
-            embed.setTitle(boss.name)
-          } 
-          if (role) {
-            embed.setTitle(guild.roles.find('name', boss.name))
-          }
+          embed.setTitle(boss.name)
           embed.setDescription(boss.alert)
           embed.setFooter(conf.region.toUpperCase())
           channel.send({ embed })
